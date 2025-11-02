b3254536a5db72e174b9d08f871901ee34efef3f
chldls153@naver.com <chldls153@gmail.com>
2025-11-02 21:21:59 +0900

    서버 액션으로 데이터 제출

--- Practice--RSC-ServerAction-Suspense/app/page.js
+++ Practice--RSC-ServerAction-Suspense/app/page.js
@@ -1,14 +1,12 @@
 import ClientDemo from '@/components/ClientDemo'
 import DataFetchingDemo from '@/components/DataFetchingDemo'
 import RSCDemo from '@/components/RSCDemo'
+import ServerActionsDemo from '@/components/ServerActionsDemo'
 
 export default function Home() {
   return (
     <main>
-      {/* <ClientDemo>
-        <RSCDemo />
-      </ClientDemo> */}
-      <DataFetchingDemo />
+      <ServerActionsDemo />
     </main>
   )
 }
