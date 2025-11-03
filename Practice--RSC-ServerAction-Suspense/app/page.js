97def8f4687df7b0fd01b8bf4ecacf49021b2815
chldls153@naver.com <chldls153@gmail.com>
2025-11-03 13:13:45 +0900

    Error 처리

--- Practice--RSC-ServerAction-Suspense/app/page.js
+++ Practice--RSC-ServerAction-Suspense/app/page.js
@@ -2,25 +2,29 @@ import ClientDemo from '@/components/ClientDemo'
 import DataFetchingDemo from '@/components/DataFetchingDemo'
 import RSCDemo from '@/components/RSCDemo'
 import ServerActionsDemo from '@/components/ServerActionsDemo'
+import fs from 'node:fs/promises'
 import UsePromiseDemo from '@/components/UsePromisesDemo'
 import { Suspense } from 'react'
-import fs from 'node:fs/promises'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
 export default function Home() {
-  const usersPromise = new Promise(resolve => {
+  const usersPromise = new Promise((resolve, reject) => {
     setTimeout(async () => {
       const data = await fs.readFile('dummy-db.json', 'utf-8')
       const users = JSON.parse(data)
 
       resolve(users)
+      // reject('Error!')
     }, 2000)
   })
 
   return (
     <main>
-      <Suspense fallback={<p>Loading users...</p>}>
-        <UsePromiseDemo usersPromise={usersPromise} />
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something went wrong!</p>}>
+        <Suspense fallback={<p>Loading users...</p>}>
+          <UsePromiseDemo usersPromise={usersPromise} />
+        </Suspense>
+      </ErrorBoundary>
     </main>
   )
 }
